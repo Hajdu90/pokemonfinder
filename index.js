538f4cbc6d168679b5a9121b8dfd8614cf914d50
Hajdu90 <hajdu90shares@gmail.com>
2025-05-10 11:14:53 +0200

    frissites vege

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const OPTIONS = {
+    api: 'https://pokeapi.co/api/v2/pokemon/'
+}
+
 const components = {
     button: 'search-button',
     input: 'search-input',
@@ -5,6 +9,40 @@ const components = {
 }
 
 
-document.getElementById(components.button).addEventListener('click',() => {
-    alert("Keresünk!")
-})
+document
+    .getElementById(components.button)
+    .addEventListener('click',async () => {
+        createPokemonImage(await getPokemonByName(
+            document.getElementById(components.input).value
+        ));
+});
+
+components.input.addEventListener('component.input', function () {
+    if (components.input.value.trim() !== ""){
+        components.button.disabled = false;
+    } else {
+        components.button.disabled = true;
+    }
+});
+
+
+
+async function getPokemonByName(pokemonName){
+    return (await fetch(`${OPTIONS.api}${pokemonName}`)).json()
+    
+
+    
+}
+// gitre
+// refactor
+// input nincs beírva gomb letiltva
+//töröljük a konténert egy kép legyen
+
+function createPokemonImage(data){
+    document.getElementById(components.container).innerHTML = '';
+    document.getElementById(components.container).append(
+        Object.assign(document.createElement("img"),{
+            src: data.sprites.front_default,
+        })
+    );
+}
